a0b6cedd4f15f50b389363b07d942e9e25de036d
dhoker23 <44202889+dhoker23@users.noreply.github.com>
2019-02-19 22:39:22 +0400

    Update code-translator.js

--- code-translator.js
+++ code-translator.js
@@ -1,1 +1,110 @@
 
+function inp(){
+    var input_text,inp_lang,out_lang,test1_1,test1_2,inp_test1;
+    input_text = document.getElementById("input").value;
+    inp_lang = document.getElementById("inplang").value;
+    out_lang = document.getElementById("out_lang").value;
+    // if(inp_lang == inp_test1_1){
+    //     if(input_text == test1){
+    //         document.getElementById("out").value = "echo('123')";
+    //     }
+
+    // }
+    switch(inp_lang)
+    {
+        case "JavaScript":
+            switch(input_text)
+        {
+            case "alert('123')":
+                switch(out_lang)
+                {
+                    case "PHP":
+                        document.getElementById("out").value = "echo('123')";
+                        break;
+                    case "C#":
+                        document.getElementById("out").value = "Console.Write('123')";
+                        break;
+                }
+            break;
+            case "var a":
+            switch(out_lang)
+                {
+                    case "PHP":
+                        document.getElementById("out").value = "$ a";
+                        break;
+                    case "C#":
+                        document.getElementById("out").value = "string/int a";
+                        break;
+                }
+            break;
+        }
+        break;
+        case "C#":
+            switch(input_text)
+        {
+            case "Console.Write('123')":
+                switch(out_lang)
+                {
+                    case "PHP":
+                        document.getElementById("out").value = "echo('123')";
+                        break;
+                    case "JavaScript":
+                        document.getElementById("out").value = "alert('123')";
+                        break;
+                }
+            break;
+            case "string a":
+            switch(out_lang)
+                {
+                    case "PHP":
+                        document.getElementById("out").value = "$ a";
+                        break;
+                    case "JavaScript":
+                        document.getElementById("out").value = "var a";
+                        break;
+                }
+            break;    
+            case "int a":
+            switch(out_lang)
+                {
+                    case "PHP":
+                        document.getElementById("out").value = "$ a";
+                        break;
+                    case "JavaScript":
+                        document.getElementById("out").value = "var a";
+                        break;
+                }
+            break;    
+                
+        }
+        break;
+        case "PHP":
+            switch(input_text)
+            {
+            case "echo('123')":
+                switch(out_lang)
+                {
+                    case "JavaScript":
+                        document.getElementById("out").value = "alert('123')";
+                        break;
+                    case "C#":
+                        document.getElementById("out").value = "Console.Write('123')";
+                        break;
+                }
+            break;    
+            case "$ a":
+            switch(out_lang)
+                {
+                    case "C#":
+                        document.getElementById("out").value = "string/int a";
+                        break;
+                    case "JavaScript":
+                        document.getElementById("out").value = "var a";
+                        break;
+                }
+            break;
+                
+        }
+        break;
+    }
+}
